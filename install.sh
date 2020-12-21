13e696afc313d153d9215a39bae12507b28c234f
knoellle <konrad@familie-noelle.de>
2020-12-21 02:23:37 +0100

    Halt install script on error

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Install packages in the correct order
 (cd libg15 && makepkg -si $@)
 (cd libg15render && makepkg -si $@)
 (cd g15daemon && makepkg -si $@)
